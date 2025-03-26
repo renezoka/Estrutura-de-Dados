5ce09459ffcab8287122ef02a8f219f49b42dbd6
renezoka <rmmedeiros300@gmail.com>
2025-03-26 17:29:33 -0300

    Update LinkedList.java

--- Est. dados/LinkedList.java
+++ Est. dados/LinkedList.java
@@ -1,5 +1,45 @@
-public class LinkedList<E> extends DynamicList<E>{
-    protected int size;
-    protected E[] list;
+public class LinkedList<E> extends DynamicList<E> {
+    @Override
+    public void add(E value) {
+        Node newNode = new Node(value);
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            newNode = tail.next;
+        }
+        tail = newNode;
+        size++;
+    }
 
+    @Override
+    public void insert(E value) {
+        Node newNode = new Node(value);
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            newNode = head.prev;
+        }
+        head = newNode;
+        size++;
+    }
+
+    @Override
+    public void insert(int index, E value){
+        if(index <= 0){
+            insert(value);
+        }else if(index >= size){
+            add(value);
+        }else{
+            Node newNode = new Node(value);
+            Node auxNode = getNodeByIndex(index-1);
+            newNode.next = auxNode.next;
+            auxNode.next = newNode;
+    }
+
+    @Override
+    public void set(int index, E value){
+        
+    }
+
+}
 }
